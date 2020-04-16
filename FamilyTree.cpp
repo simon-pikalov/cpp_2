53401047f53e6349ce5481ebe4103ce984b539d7
Simon Pikalov <simon96pikalov@gmail.com>
2020-04-16 15:05:08 +0300

    test

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -3,3 +3,28 @@
 #include <iostream>
 using namespace family;
 using namespace std;
+
+
+Tree& Tree::addFather(string to, string name){
+    return *this;
+}
+
+Tree& Tree::addMother(string to, string name){
+    return *this;
+}
+
+void Tree::display(){
+// "TO_DO";
+}
+
+string Tree::relation(string name){
+    return "TO_DO";
+}
+
+string Tree::find(string relation){
+   return "TO_DO";
+}
+
+void Tree::remove(string name){
+
+}
