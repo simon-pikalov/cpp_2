8f0beb55667d9946cd21f04f3e6d044dc624cc09
Simon Pikalov <simon96pikalov@gmail.com>
2020-04-16 15:25:43 +0300

    test

--- Demo.cpp
+++ Demo.cpp
@@ -1,17 +1,10 @@
-/**
- * Demo for Family Tree
- * 
- * First version by Adam Lev-Ari,  2019-12
- * Second version by Erel Segal-Halevi, 2020-04
- */
-
 #include "FamilyTree.hpp"
-using namespace family;
+
 #include <iostream>
-using namespace std;
+using namespace std; 
 
 int main() {
-	Tree T ("Yosef"); // Yosef is the "root" of the tree (the youngest person).
+	family::Tree T ("Yosef"); // Yosef is the "root" of the tree (the youngest person).
 	T.addFather("Yosef", "Yaakov")   // Tells the tree that the father of Yosef is Yaakov.
 	 .addMother("Yosef", "Rachel")   // Tells the tree that the mother of Yosef is Rachel.
 	 .addFather("Yaakov", "Isaac")
@@ -30,7 +23,7 @@ int main() {
 	cout << T.relation("Yosef") << endl;  // prints "me"
 
 	cout << T.find("mother") << endl;  // prints "Rachel"
-	cout << T.find("great-great-grandfather") << endl;  // prints "Terah"
+	cout << T.find("great-great-grandfather") << endl;  // prints "Avraham"
 	try {
 		cout << T.find("uncle") << endl;  // throws an exception
 	} catch (const exception& ex) {
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -11,13 +11,10 @@ class Tree {       // The class
     Tree* mother;           // Right Node
 
     Tree(string name) {
-    Tree::me=me;
+    
     }
 
     Tree(string name,Tree* father, Tree* mother) {
-    Tree::me=me;
-    Tree::father=father;
-    T:mother=mother;
     }
 
     Tree& addFather(string prsoon, string father);
