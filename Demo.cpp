8764597506464bfac0e3770dff42d1388701f254
Simon Pikalov <simon96pikalov@gmail.com>
2020-04-16 15:17:35 +0300

    test

--- Demo.cpp
+++ Demo.cpp
@@ -6,12 +6,12 @@
  */
 
 #include "FamilyTree.hpp"
-
+using namespace family;
 #include <iostream>
 using namespace std;
 
 int main() {
-	family::Tree T ("Yosef"); // Yosef is the "root" of the tree (the youngest person).
+	Tree T ("Yosef"); // Yosef is the "root" of the tree (the youngest person).
 	T.addFather("Yosef", "Yaakov")   // Tells the tree that the father of Yosef is Yaakov.
 	 .addMother("Yosef", "Rachel")   // Tells the tree that the mother of Yosef is Rachel.
 	 .addFather("Yaakov", "Isaac")
